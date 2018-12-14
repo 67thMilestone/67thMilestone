4f200c6e5e1159364652291583214cb662163718
Shrey Dixit <shrey.dixit.17cse@bml.edu.in>
2018-12-14 21:43:27 +0530

    Added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,112 +1,152 @@
-Barba.Pjax.start();
-Barba.Prefetch.init()
-let Elclass;
+$(document).ready(function () {
+    Barba.Pjax.start();
+    Barba.Prefetch.init();
+    let Elclass;
 
-var transitionAnimation1 = Barba.BaseTransition.extend({
-    start: function () {
-        Promise
-            .all([this.newContainerLoading, this.startTransition()])
-            .then(this.fadeIn.bind(this));
-    },
+    let options = {
+        damping: 0.04,
+        thumbMinSize: 20,
+        continuousScrolling: false
+    };
+    let Scrollbar = window.Scrollbar;
+    Scrollbar.init(document.querySelector('body'), options);
 
-    startTransition: function () {
-        let deferred = Barba.Utils.deferred();
-        $('html,body').animate({ scrollTop: 0 }, 'slow');
-        var outTransition = new TimelineMax();
-        outTransition
-            //.to(window, 1, {scrollTo:0})
-            .set(".white-wipe", { display: 'block', y: "0%" })
-            .staggerFromTo(".white-wipe", 1, { width: "0%" }, {
-                width: "100%",
-                ease: Power3.easeOut,
-                onComplete: () => { deferred.resolve(); }
-            }, 0.1);
-        return deferred.promise;
-    },
+    var transitionAnimation1 = Barba.BaseTransition.extend({
+        start: function () {
+            Promise
+                .all([this.newContainerLoading, this.startTransition()])
+                .then(this.fadeIn.bind(this));
+        },
 
-    fadeIn: function () {
-        var _this = this;
-        var $el = $(this.newContainer);
-        var outTransition = new TimelineMax();
-        outTransition
-            .set($(this.oldContainer), { display: "none", delay: 0.5 })
-            .set($el, { visibility: "visible", opacity: 0 })
-            .staggerFromTo(".white-wipe", 1, { width: "100%" }, { width: "0px", ease: Power3.easeIn, delay: 0.2 }, 0.2)
-            .set(".white-wipe", { display: 'none' })
-            .to($el, 0.7, {
-                opacity: 1,
-                onComplete: function () {
-                    _this.done();
-                }
-            }, "-=0.4");
-    }
-});
+        startTransition: function () {
+            let deferred = Barba.Utils.deferred();
+            $('html,body').animate({ scrollTop: 0 }, 'slow');
+            var outTransition = new TimelineMax();
+            outTransition
+                //.to(window, 1, {scrollTo:0})
+                .set(".white-wipe", { display: 'block', y: "0%" })
+                .staggerFromTo(".white-wipe", 1, { width: "0%" }, {
+                    width: "100%",
+                    ease: Power3.easeOut,
+                    onComplete: () => { deferred.resolve(); }
+                }, 0.1);
+            return deferred.promise;
+        },
 
-var transitionAnimation2 = Barba.BaseTransition.extend({
-    start: function () {
-        Promise
-            .all([this.newContainerLoading, this.startTransition()])
-            .then(this.fadeIn.bind(this));
-    },
-
-    startTransition: function () {
-        let deferred = Barba.Utils.deferred();
-        $('html,body').animate({ scrollTop: 0 }, 'slow');
-        var outTransition = new TimelineMax();
-        outTransition
-            //.to(document.body, 1, {scrollTo: 0,})
-            .set(".Wipe", { display: 'block', y: "-100%", onComplete: () => { deferred.resolve(); } })
-        return deferred.promise;
-    },
-
-    fadeIn: function () {
-        var _this = this;
-        var $el = $(this.newContainer);
-        var outTransition = new TimelineMax();
-        outTransition
-            .to(".Wipe", 1, { y: "0%", ease: Power3.easeIn, delay: "0.2" })
-            .set($(this.oldContainer), { display: "none" }, "-=0.1")
-            .set($el, { visibility: "visible", opacity: 0 }, "-=0.1")
-            .to(".Wipe", 1, { y: "105%", ease: Sine.easeOut, })
-            .to($el, 1, {
-                opacity: 1,
-                onStart: function () {
-                    _this.done();
-                }
-            }, "-=0.7")
-            .set(".Wipe", { display: 'none' });
-    }
-});
-
-Barba.Dispatcher.on('linkClicked', function (HTMLElement, MouseEvent) {
-    Elclass = $(HTMLElement).attr('class');
-    console.log("inside " + Elclass)
-});
-
-Barba.Pjax.getTransition = function () {
-    console.log("outside " + Elclass)
-    if (Elclass == "t1")
-        return transitionAnimation1;
-    return transitionAnimation2;
-};
-function splitLetters($var) {
-    $($var).each(function () {
-        $(this).html(
-            $(this)
-                .text()
-                .replace(/([^\x00-\xFF]|\w)/g, "<span class='letter'>$&</span>"),
-        );
+        fadeIn: function () {
+            var _this = this;
+            var $el = $(this.newContainer);
+            var outTransition = new TimelineMax();
+            outTransition
+                .set($(this.oldContainer), { display: "none", delay: 0.5 })
+                .set($el, { visibility: "visible", opacity: 0 })
+                .staggerFromTo(".white-wipe", 1, { width: "100%" }, { width: "0px", ease: Power3.easeIn, delay: 0.2 }, 0.2)
+                .set(".white-wipe", { display: 'none' })
+                .to($el, 0.7, {
+                    opacity: 1,
+                    onComplete: function () {
+                        _this.done();
+                    }
+                }, "-=0.4");
+        }
     });
-}
-splitLetters($('.animateText'));
 
-function animateText() {
-    TweenMax.staggerFromTo('.textAnimation1 .letter', 2.25, { opacity: 0 }, { opacity: 1, ease: Power4.easeInOut }, 0.15);
-    TweenMax.staggerFromTo('.textAnimation2 .letter', 0.75, { css: { transform: 'translateY(-100px)' } }, { css: { transform: 'translateY(0px)' }, ease: Expo.easeOut }, 0.05);
-    TweenMax.staggerFromTo('.textAnimation3 .letter', 1.4, { css: { transform: 'translateY(100px) translateZ(0)', opacity: 0 } }, { css: { transform: 'translateY(0px) translateZ(0)', opacity: 1 }, delay: 0.3 , ease: Expo.easeOut}, 0.045);
-    TweenMax.staggerFromTo('.textAnimation4 .letter', 1.5, { css: { transform: 'scale(0)' } }, { css: { transform: 'scale(1)' }, ease: Elastic.easeOut.config(1, 0.3) }, 0.045);
-    TweenMax.staggerFromTo('.textAnimation5 .letter', 1.2, { css: { transform: 'translateX(40px) translateZ(0)', opacity: 0 } }, { css: { transform: 'translateX(0px) translateZ(0)', opacity: 1 }, ease: Expo.easeOut, delay: 0.5 }, 0.04);
-}
+    var transitionAnimation2 = Barba.BaseTransition.extend({
+        start: function () {
+            Promise
+                .all([this.newContainerLoading, this.startTransition()])
+                .then(this.fadeIn.bind(this));
+        },
 
-setInterval(animateText, 5000);
+        startTransition: function () {
+            let deferred = Barba.Utils.deferred();
+            $('html,body').animate({ scrollTop: 0 }, 'slow');
+            var outTransition = new TimelineMax();
+            outTransition
+                //.to(document.body, 1, {scrollTo: 0,})
+                .set(".Wipe", { display: 'block', y: "-100%", onComplete: () => { deferred.resolve(); } })
+            return deferred.promise;
+        },
 
+        fadeIn: function () {
+            var _this = this;
+            var $el = $(this.newContainer);
+            var outTransition = new TimelineMax();
+            outTransition
+                .to(".Wipe", 1, { y: "0%", ease: Power3.easeIn, delay: "0.2" })
+                .set($(this.oldContainer), { display: "none" }, "-=0.1")
+                .set($el, { visibility: "visible", opacity: 0 }, "-=0.1")
+                .to(".Wipe", 1, { y: "105%", ease: Sine.easeOut, })
+                .to($el, 1, {
+                    opacity: 1,
+                    onStart: function () {
+                        _this.done();
+                    }
+                }, "-=0.7")
+                .set(".Wipe", { display: 'none' });
+        }
+    });
+
+    Barba.Dispatcher.on('linkClicked', function (HTMLElement, MouseEvent) {
+        Elclass = $(HTMLElement).attr('class');
+        console.log("inside " + Elclass)
+    });
+
+    Barba.Pjax.getTransition = function () {
+        console.log("outside " + Elclass)
+        if (Elclass == "t1")
+            return transitionAnimation1;
+        return transitionAnimation2;
+    };
+    function splitLetters($var) {
+        $($var).each(function () {
+            $(this).html(
+                $(this)
+                    .text()
+                    .replace(/([^\x00-\xFF]|\w)/g, "<span class='letter'>$&</span>"),
+            );
+        });
+    }
+    splitLetters($('.animateText'));
+
+    let textAnimation1 = TweenMax.staggerFromTo('.textAnimation1 .letter', 2.25, { opacity: 0 }, { opacity: 1, ease: Power4.easeInOut }, 0.15);
+    let textAnimation2 = TweenMax.staggerFromTo('.textAnimation2 .letter', 0.75, { css: { transform: 'translateY(-100px)' } }, { css: { transform: 'translateY(0px)' }, ease: Expo.easeOut }, 0.05);
+    let textAnimation3 = TweenMax.staggerFromTo('.textAnimation3 .letter', 1.4, { css: { transform: 'translateY(100px) translateZ(0)', opacity: 0 } }, { css: { transform: 'translateY(0px) translateZ(0)', opacity: 1 }, delay: 0.3, ease: Expo.easeOut }, 0.045);
+    let textAnimation4 = TweenMax.staggerFromTo('.textAnimation4 .letter', 1.5, { css: { transform: 'scale(0)' } }, { css: { transform: 'scale(1)' }, ease: Elastic.easeOut.config(1, 0.3) }, 0.045);
+    let textAnimation5 = TweenMax.staggerFromTo('.textAnimation5 .letter', 1.2, { css: { transform: 'translateX(40px) translateZ(0)', opacity: 0 } }, { css: { transform: 'translateX(0px) translateZ(0)', opacity: 1 }, ease: Expo.easeOut, delay: 0.5 }, 0.04);
+
+    let controller = new ScrollMagic.Controller();
+
+    var scene = new ScrollMagic.Scene({
+        triggerElement: ".textAnimation1",
+        triggerHook: 1
+    })
+    .setTween(textAnimation1) // trigger a TweenMax.to tween
+    .addTo(controller);
+    var scene = new ScrollMagic.Scene({
+        triggerElement: ".textAnimation2",
+        triggerHook: 1
+    })
+    .setTween(textAnimation2) // trigger a TweenMax.to tween
+    .addTo(controller);
+    var scene = new ScrollMagic.Scene({
+        triggerElement: ".textAnimation3",
+        triggerHook: 1
+    })
+    .setTween(textAnimation3) // trigger a TweenMax.to tween
+    .addTo(controller);
+    var scene = new ScrollMagic.Scene({
+        triggerElement: ".textAnimation4",
+        triggerHook: 1
+    })
+    .setTween(textAnimation4) // trigger a TweenMax.to tween
+    .addTo(controller);
+    var scene = new ScrollMagic.Scene({
+        triggerElement: ".textAnimation5",
+        triggerHook: 1
+    })
+    .setTween(textAnimation5) // trigger a TweenMax.to tween
+    .addTo(controller);
+
+    //setInterval(animateText, 5000);
+});
